042a62f1fc2c5a605929a4333d01e83ce3352357
Ryan Owens <RyanOwens@linux.com>
2020-07-17 14:19:34 -0500

    feat: added service c 	- Set default authorization to deny-all 	- Allow public access to services A and B 	- Allow Service A to access Service C

--- servicea/main.py
+++ servicea/main.py
@@ -1,8 +1,15 @@
 from typing import Optional
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
+def get_c():
+    response = requests.get("http://servicec:3000")
+    print(response)
+    return response.json()
+
 @app.get("/")
 def root():
-    return { "Service": "A" }
+    c = get_c()
+    return { "Service": "A", "C": c }
